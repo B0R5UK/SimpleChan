7d9ce588ef76714b4c2418d156b579d098fde590
Borsuk <37664860+B0R5UK@users.noreply.github.com>
2018-05-19 16:41:20 +0200

    Added ip logging

--- views/routes/comments.js
+++ views/routes/comments.js
@@ -3,6 +3,15 @@ var router = express.Router();
 var Post = require("../../models/post.js");
 var Comment = require("../../models/comment.js");
 var multer  = require('multer');
+var get_ip = require('ipware')().get_ip;
+//GET IP
+router.use(function(req, res, next) {
+     var ip_info = get_ip(req);
+     console.log(ip_info);
+     // { clientIp: '127.0.0.1', clientIpRoutable: false }
+     next();
+ });
+
 
 //MULTER
 var storage = multer.diskStorage({
@@ -13,7 +22,7 @@ var storage = multer.diskStorage({
     let ext = file.originalname.substring(file.originalname.lastIndexOf('.'), file.originalname.length);
     cb(null, Date.now() + ext);
   }
-})
+});
 //MULTER
 
 var upload = multer({ storage: storage });
@@ -21,7 +30,8 @@ var upload = multer({ storage: storage });
 
 router.post("/:boardname/:postid/newcomment",upload.single('commentimage'), function(req, res){
     var imagesrc = "/uploads/" + req.file.filename;
-    var newComment={author: req.body.author, image: imagesrc, text: req.body.text};
+    var getip=get_ip(req);
+    var newComment={ipadress:getip.clientIP,author: req.body.author, image: imagesrc, text: req.body.text};
     Post.findById(req.params.postid, function(err, post) {
        if(err){
            console.log("error finding post" + err);
