b365f2168c105c0baa496362e381d27058df14f4
Borsuk <37664860+B0R5UK@users.noreply.github.com>
2018-05-23 08:47:26 +0200

    Added publlic directories

--- app.js
+++ app.js
@@ -2,19 +2,25 @@ var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
+var path = require('path');
 // var Post = require("/models/post.js");
 // var Comment = require("./models/comment.js");
 // var Board = require("./models/board.js");
 var multer  = require('multer');
 var createBoards = require("./seed.js");
+var path = require("path");
+console.log(path.dirname(process.execPath));
 
 //DATABASE
-mongoose.connect("mongodb://localhost/blogtest");
+mongoose.connect("mongodb://localhost/simplechan");
 
-//createBoards();
+createBoards();
 
 app.use(bodyParser.urlencoded({extended: true})); //bodyparser
-app.use(express.static(__dirname + "/public")); //static folder
+
+app.use('*/javascript',express.static(path.join(__dirname, 'public/javascript')));
+app.use('*/stylesheets',express.static(path.join(__dirname, 'public/stylesheets')));
+app.use('*/uploads',express.static(path.join(__dirname, 'public/uploads')));
 
 var postsRoutes = require("./views/routes/posts.js");
 var commentRoutes = require("./views/routes/comments.js");
@@ -22,6 +28,6 @@ var commentRoutes = require("./views/routes/comments.js");
 app.use(postsRoutes);
 app.use(commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
-console.log("server started!");
+app.listen('3000', function(){
+console.log("server started on port 3000");
 });
